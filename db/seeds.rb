29dd4aa8430a78f4bf19d59098bf3e5af06990b7
Erwin Nguyen <erwin.nguyen@gmail.com>
2015-10-03 08:20:47 -0700

    Assignment 33 Seeding Data Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,18 @@ posts = Post.all
   )
 end
 
+# Create a Unique Post
+Post.find_or_create_by(
+  title: "This is a unique post",
+  body: "Just one of a kind post"
+)
+
+# Create a Unique comment
+Comment.find_or_create_by(
+  post: posts.sample,
+  body: "First time commenting!"
+)
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
