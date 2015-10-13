d4ad370883cab74443f85a14e88957e221ce736f
Erwin Nguyen <erwin.nguyen@gmail.com>
2015-10-12 17:02:11 -0700

    Checkpoint 43 Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
 include RandomData
 
+# Create users
+5.times do
+  user = User.create!(
+  name:      RandomData.random_name,
+  email:     RandomData.random_email,
+  password:  RandomData.random_sentence
+  )
+end
+users = User.all
+
 #Create topics
 15.times do
   Topic.create!(
@@ -12,9 +22,10 @@ topics = Topic.all
 # Create Posts
 50.times do
   Post.create!(
+    user:  users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
+    body:  RandomData.random_paragraph
   )
 end
 posts = Post.all
@@ -27,8 +38,15 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'erwin.nguyen@gmail.com',
+  password: 'helloworld'
+)
+
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,8 +5,9 @@ include RandomData
 
 RSpec.describe Comment, type: :model do
   let(:topic) {Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
-  let(:post) {topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph)}
-  let(:comment){Comment.create!(body: 'Comment Body', post: post)}
+  let(:post) {topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)}
+  let(:comment) {Comment.create!(body: 'Comment Body', post: post)}
+  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
 
   describe "attributes" do
     it "should respond to body" do
