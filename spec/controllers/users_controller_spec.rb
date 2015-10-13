f453a136b952e8fc08f6e5af7be7d161f9c71646
Erwin Nguyen <erwin.nguyen@gmail.com>
2015-10-13 02:29:56 -0700

    Checkpoint 44 completed

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   let (:new_user_attributes) do
     {
-      name: "BlockHead",
+      name: "BlocHead",
       email: "blochead@bloc.io",
       password: "blochead",
       password_confirmation: "blochead"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,6 +37,48 @@ RSpec.describe User, type: :model do
       post :create, user: new_user_attributes
       expect(session[:user_id]).to eq assigns(:user).id
     end
+
+    it "should respond to role" do
+      expect(user).to respond_to(:role)
+    end
+
+    it "should respond to admin?" do
+      expect(user).to respond_to(:admin?)
+    end
+
+    it "should respond to member?" do
+      expect(user).to respond_to(:member?)
+    end
+  end
+
+  describe "roles" do
+    it "should be member by default" do
+      expect(user.role).to eql("member")
+    end
+
+    context "member user" do
+      it "should return true for #member?" do
+        expect(user.member?).to be_truthy
+      end
+
+      it "should return false for #admin?" do
+        expect(user.admin?).to be_falsey
+      end
+    end
+
+    context "admin user" do
+      before do
+        user.admin!
+      end
+
+      it "should return false for #member?" do
+        expect(user.member?).to be_falsey
+      end
+
+      it "should return true for #admin?" do
+        expect(user.admin?).to be_truthy
+      end
+    end
   end
 
   describe "invalid user" do
