2948bb1ecdf4252cc9c8a188164c3f6276aacf36
Erwin Nguyen <erwin.nguyen@gmail.com>
2015-10-22 13:01:40 -0700

    inprogress cp

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ end
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.0'
+  gem 'factory_girl_rails', '~> 4.0'
+
 end
 
 group :test do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,5 +28,8 @@ class UsersController < ApplicationController
     @user.password_confirmation = params[:user][:password_confirmation]
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
 
 end
