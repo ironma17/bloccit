e135aec0a5b63d87e7fd8ab020d3db1a44d66a98
Erwin Nguyen <erwin.nguyen@gmail.com>
2015-10-26 18:57:42 -0700

    Completed assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
     @post.user = current_user
 
     if @post.save
-      @post.rating = Rating.update_rating(params[:post][:ratings])
+      @post.rating = Rating.update_rating(params[:post][:rating])
       @post.labels = Label.update_labels(params[:post][:labels])
       flash[:notice] = "Post was saved."
       redirect_to [@topic, @post]
